e1365a9c806e54839b375bcb7e7e2b76f313a993
TobiasEvjuGit <49237249+TobiasEvjuGit@users.noreply.github.com>
2019-09-11 14:53:43 +0200

    Add files via upload

---
+++ test/src/Oblig1.java
@@ -0,0 +1,101 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+
+public class Oblig1 {
+    private Oblig1() {
+    }
+
+    ///// Oppgave 1 //////////////////////////////////////
+    public static int maks(int[] a) {
+        try {
+            if (a.length == 0) {
+                throw new NoSuchElementException("Antall elementer er 0");
+            }
+            int temp;
+
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                }
+            }
+            return a[a.length - 1];
+        } catch (NoSuchElementException e) {
+            System.err.println(e.getMessage());
+        }
+        return -1;
+    }
+
+    public static int ombyttinger(int[] a) {
+
+
+
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 2 //////////////////////////////////////
+    public static int antallUlikeSortert(int[] a) {
+        throw new NotImplementedException();
+    }
+
+
+    ///// Oppgave 3 //////////////////////////////////////
+    public static int antallUlikeUsortert(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 4 //////////////////////////////////////
+    public static void delsortering(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 5 //////////////////////////////////////
+    public static void rotasjon(char[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 6 //////////////////////////////////////
+    public static void rotasjon(char[] a, int k) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 7 //////////////////////////////////////
+    /// 7a)
+    public static String flett(String s, String t) {
+        throw new NotImplementedException();
+    }
+
+    /// 7b)
+    public static String flett(String... s) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 8 //////////////////////////////////////
+    public static int[] indekssortering(int[] a) {
+        throw new NotImplementedException();
+    }
+
+
+    ///// Oppgave 9 //////////////////////////////////////
+    public static int[] tredjeMin(int[] a) {
+        throw new NotImplementedException();
+    }
+
+    ///// Oppgave 10 //////////////////////////////////////
+    public static int bokstavNr(char bokstav) {
+        throw new NotImplementedException();
+    }
+
+    public static boolean inneholdt(String a, String b) {
+        throw new NotImplementedException();
+    }
+
+
+
+
+
+}  // Oblig1
